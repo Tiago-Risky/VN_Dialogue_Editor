f2ce05903c877cae79d7ebbee5048a5ead67c86c
Tiago Cabral <riskybomberpt@gmail.com>
2020-05-23 22:05:06 +0100

    [INIT] First Upload
    Basic XML loading, saving and editing.
    First Upload

---
+++ VNDialogueEditor/MainWindow.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace VNDialogueEditor
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openXMLFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportXMLFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 24);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(454, 472);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.TreeView1_AfterSelect);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(954, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openXMLFileToolStripMenuItem,
+            this.exportXMLFileToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openXMLFileToolStripMenuItem
+            // 
+            this.openXMLFileToolStripMenuItem.Name = "openXMLFileToolStripMenuItem";
+            this.openXMLFileToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.openXMLFileToolStripMenuItem.Text = "Open XML File";
+            this.openXMLFileToolStripMenuItem.Click += new System.EventHandler(this.OpenXMLFile_Click);
+            // 
+            // exportXMLFileToolStripMenuItem
+            // 
+            this.exportXMLFileToolStripMenuItem.Name = "exportXMLFileToolStripMenuItem";
+            this.exportXMLFileToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.exportXMLFileToolStripMenuItem.Text = "Export XML File";
+            this.exportXMLFileToolStripMenuItem.Click += new System.EventHandler(this.ExportXMLFile_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.SaveButton);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(454, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 472);
+            this.panel1.TabIndex = 5;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(413, 42);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 4;
+            this.SaveButton.Text = "Save Edit";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "XML";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(57, 44);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(350, 175);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 399);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Raw XML";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 293);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(425, 167);
+            this.textBox1.TabIndex = 0;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 496);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(950, 500);
+            this.Name = "MainWindow";
+            this.Text = "VN Dialogue Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openXMLFileToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ToolStripMenuItem exportXMLFileToolStripMenuItem;
+    }
+}
+
---
+++ VNDialogueEditor/MainWindow.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace VNDialogueEditor {
+    public partial class MainWindow : Form {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private XElement XmlFile;
+
+        private void LoadXML() {
+            OpenFileDialog openFileDialog = new OpenFileDialog {
+                Filter = "XML Files (*.xml)|*.xml|All files (*.*)|*.*",
+                FilterIndex = 1
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                XmlFile = XElement.Load(openFileDialog.FileName);
+                PopulateTree(treeView1, XmlFile);
+            }
+        }
+
+        private void ExportXML() {
+            Stream fileStream;
+            SaveFileDialog saveFileDialog = new SaveFileDialog {
+                Filter = "XML Files (*.xml)|*.xml|All files (*.*)|*.*",
+                FilterIndex = 1
+            };
+            if (saveFileDialog.ShowDialog() == DialogResult.OK) {
+                if ((fileStream = saveFileDialog.OpenFile()) != null) {
+                    StreamWriter streamWriter = new StreamWriter(fileStream);
+                    streamWriter.Write(treeView1.Nodes[0].Tag.ToString());
+                    streamWriter.Flush();
+                    fileStream.Position = 0;
+                    fileStream.Close();
+                }
+                else {
+                    MessageBox.Show("Can't write the file.", "Error writing file",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        public void PopulateTree(TreeView treeview, XElement xmlDocument) {
+            treeview.Nodes.Clear();
+            treeview.Enabled = false;
+            TreeNode Document = new TreeNode("Document");
+            Document.Tag = xmlDocument;
+            treeview.Nodes.Add(Document);
+            AddNodes(Document, xmlDocument);
+            treeview.ExpandAll();
+            treeview.Nodes[0].EnsureVisible();
+            treeview.Enabled = true;
+        }
+
+        private void AddNodes(TreeNode treeNode, XElement parent) {
+            foreach (XElement child in parent.Elements()) {
+                TreeNode childNode = treeNode.Nodes.Add(child.Name.ToString());
+                childNode.Tag = child;
+                AddNodes(childNode, child);
+            }
+        }
+
+        private void RebuildNodes(TreeNode treeNode) {
+            while (treeNode.Parent != null && treeNode.Parent.Tag != null) {
+                XElement parentElement = (XElement)treeNode.Parent.Tag;
+                parentElement.RemoveNodes();
+                foreach (TreeNode node in treeNode.Parent.Nodes) {
+                    parentElement.Add((XElement)node.Tag);
+                }
+                treeNode = treeNode.Parent;
+            }
+        }
+
+        private void TreeView1_AfterSelect(object sender, TreeViewEventArgs e) {
+            textBox1.Text = treeView1.SelectedNode.Tag.ToString();
+            textBox2.Text = treeView1.SelectedNode.Tag.ToString();
+        }
+
+        private void OpenXMLFile_Click(object sender, EventArgs e) {
+            LoadXML();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e) {
+            treeView1.SelectedNode.Tag = XElement.Parse(textBox2.Text);
+            RebuildNodes(treeView1.SelectedNode);
+        }
+
+        private void ExportXMLFile_Click(object sender, EventArgs e) {
+            ExportXML();
+        }
+    }
+}
